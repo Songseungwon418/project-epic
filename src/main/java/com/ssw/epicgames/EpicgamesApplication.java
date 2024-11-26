f28ddc3fb9f7ab1036c7f1d050bfb5edafa291f1
Songseungwon418 <ttig0614@naver.com>
2024-11-26 15:20:45 +0900

    feat : first commit - Spring Boot 프로젝트 빌드

---
+++ src/main/java/com/ssw/epicgames/EpicgamesApplication.java
@@ -0,0 +1,13 @@
+package com.ssw.epicgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicgamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicgamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssw/epicgames/EpicgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssw.epicgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicgamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
