954f33f73ba098849da4ad908c99bcad0d1ef732
mnuw2626 <mnuw2626@naver.com>
2024-12-09 16:39:14 +0900

    구매 관련 페이지 작성 및 수정 - 장바구니페이지 - 위시리스트페이지 - 결제페이지 -> 결제 방법 목록 추가해야함

--- src/main/java/com/ssw/epicgames/controllers/PurchaseController.java
+++ src/main/java/com/ssw/epicgames/controllers/PurchaseController.java
@@ -9,10 +9,36 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value = "/purchase")
 public class PurchaseController {
+
+    //region 장바구니 관련
     @GetMapping(value = "/cart", produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView getIndex() {
+    public ModelAndView getCart() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("purchase/cart");
         return mav;
     }
+
+    //endregion
+
+    //region 위시리스트 관련
+    @GetMapping(value = "/wishlist", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getWishlis() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("purchase/wishlist");
+        return mav;
+    }
+
+    //endregion
+
+    //region 결제 관련
+    @GetMapping(value = "/pay", produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getPay() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("purchase/pay");
+        return mav;
+    }
+    //endregion
+
+
+
 }
