c1c5e0fbbcffdaf261a1b04de730756c62a08127
mnuw2626 <mnuw2626@naver.com>
2024-12-16 14:35:46 +0900

    24.12.16 가격 관련된 기능 추가 - 할인 정보를 포함하고 할인된 가격을 계산하는 PriceVo 생성 - PriceVo를 이용하여 게임의 가격 및 할인 정보를 반환하는 서비스 생성 - 장바구니, 위시리스트 할인 정보를 포함한 가격을 보여주고, 전체 가격 및 전체 할인 가격을 보여주게 수정

---
+++ src/main/java/com/ssw/epicgames/vos/PriceVo.java
@@ -0,0 +1,35 @@
+package com.ssw.epicgames.vos;
+
+import com.ssw.epicgames.entities.DiscountEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceVo extends DiscountEntity {
+    int originalPrice; // 게임 본래 가격
+    int discountPrice; // 할인 가격
+    int currentPrice; // 현제 게임 가격(할인되면 할인된 가격, 아님 본래 가격)
+    int discountPer; // 할인 퍼센트를 정수로
+
+
+    /** 게임 본 가격과 할인률을 받아서 실제 가격을 계산 */
+    public PriceVo(int originalPrice, float discountPer) {
+        this.originalPrice = originalPrice;
+        super.setDiscountPercent(discountPer);
+        this.discountPer = (int)discountPer;
+
+        // discountPer가 0일 경우, 할인 금액을 0으로 설정
+        if (discountPer == 0) {
+            this.discountPrice = 0;
+            this.currentPrice = originalPrice;  // 할인 없으므로 원 가격 그대로
+        } else { // discountPer가 0이 아닐 때 할인 금액 계산
+            this.discountPrice = (int)(originalPrice * (discountPer / 100));// originalPrice 에 discountPer(할인률)을 적용하여 할인되는 금액
+            this.currentPrice = originalPrice - discountPrice; // 오리지널 가격에 할인 금액을 뺀 가격
+        }
+    }
+}
