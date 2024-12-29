1f0644a6807c3d531a5433ae075f9bce9f5f199a
mnuw2626 <mnuw2626@naver.com>
2024-12-29 20:58:10 +0900

    241229 - 게임테이블 삽입 매퍼 구성 - config 폴더 생성 및 WedConfig 클래스 생성하여 리소스의 /static 아래 경로에 있는 데이터들을 캐시에 저장하여 로딩되는 속도를 미미하게 개선

---
+++ src/main/java/com/ssw/epicgames/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.ssw.epicgames.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600);  // 1시간 동안 캐시
+    }
+}
